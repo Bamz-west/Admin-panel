ef42d39679bb30e65dd1a60c270035b0e5c0b2df
bamz-west <fabamiseadeolu@gmail.com>
2022-06-18 02:43:07 +0100

    first commit

---
+++ src/pages/Charts/Bar.jsx
@@ -0,0 +1,36 @@
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Tooltip, ColumnSeries, DataLabel, Category } from "@syncfusion/ej2-react-charts";
+import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from "../../data/dummy";
+
+import { Header } from "../../components";
+import { useStateContext } from "../../context/ContextProvider";
+
+const Bar = () => {
+
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Bar" title="Olympic Medal Counts - RIO" />
+
+      <ChartComponent
+        id="bar-chart"
+        height="420px"
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 }}}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "33373E" : "#fff"}
+      >
+        <Inject services={[ColumnSeries, DataLabel, Tooltip, Legend, Category]} />
+
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  )
+}
+
+export default Bar;
